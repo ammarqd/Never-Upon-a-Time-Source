0638786886b2911e38e6c8251b17f3c21cf67c84
AmmarQ <ammar-g@hotmail.co.uk>
2023-12-19 19:49:44 +0000

    Add player movement, player direction indicator, basic camera movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour {
-    public Vector2 moveValue ;
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    public Vector2 moveValue;
     public float speed;
+    public float rotationSpeed;
 
-    void OnMove(InputValue value) {
-        moveValue = value.Get<Vector2>();
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
     }
-
-    void FixedUpdate() {
+    void OnMove(InputValue value)
+    {
+        moveValue = value.Get<Vector2>();
+    }    
+    void FixedUpdate()
+    {
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
+        controller.Move(movement * speed * Time.fixedDeltaTime);
 
-        GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
+        if (movement != Vector3.zero)
+        {
+            // calculate the rotation the player should have to face towards the movement direction
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
     }
 }
