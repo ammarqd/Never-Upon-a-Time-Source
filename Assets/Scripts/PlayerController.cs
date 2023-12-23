5236e7903fbecdb253a0f53cc28b023fcb7117a6
mayarsim <mayars7@gmail.com>
2023-12-24 02:46:52 +0300

    Add jump function

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,28 +4,69 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private CharacterController controller;
-    public Vector2 moveValue;
-    public float speed;
-    public float rotationSpeed;
+
+    private Vector2 moveValue;
+    private Vector3 playerVelocity;
+    private bool isJumping;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private float rotationSpeed;
+
+    [SerializeField]
+    private float jumpHeight = 1.0f;
+
+    [SerializeField]
+    private float gravityValue = -9.81f;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
     }
+
     void OnMove(InputValue value)
     {
         moveValue = value.Get<Vector2>();
-    }    
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (controller.isGrounded && value.isPressed)
+        {
+            isJumping = true;
+        }
+    }
+
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
-        controller.Move(movement * speed * Time.fixedDeltaTime);
+
+        // Handle horizontal movement
+        controller.Move(movement * moveSpeed * Time.fixedDeltaTime);
 
         if (movement != Vector3.zero)
         {
             // calculate the rotation the player should have to face towards the movement direction
             Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+
+        // Handle vertical movement (Jumping and Gravity)
+        if (isJumping)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            isJumping = false;
+        }
+
+        playerVelocity.y += gravityValue * Time.fixedDeltaTime;
+        controller.Move(playerVelocity * Time.fixedDeltaTime);
+
+        // Reset player's vertical velocity when grounded
+        if (controller.isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
         }
     }
 }
